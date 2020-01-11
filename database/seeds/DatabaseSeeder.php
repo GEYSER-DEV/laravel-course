c117adc5cd4b42eb69f9a7b5080d17f9f34b39ae
geyser-dev <davidalayon10@hotmail.com>
2020-01-11 16:59:27 -0600

    Modelos & Seeder & Factory Archivos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -9,8 +10,25 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->truncateTablas([
+            'rol',
+            'permiso'
+        ]);
+        $this->call(TablaRolSeeder::class);
+        $this->call(TablaPermisoSeeder::class);
     }
+
+    protected function truncateTablas(array $tablas){
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach ($tablas as $tabla) {
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+
+    }
+
 }
