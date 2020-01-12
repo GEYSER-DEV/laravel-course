73026c4dc8ac410b43635d9dc05256388e47c29d
geyser-dev <davidalayon10@hotmail.com>
2020-01-11 19:16:52 -0600

    Routes & View

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ //Route::get('permiso/{nombre}','PermisoController@index');
+
+//Route::view('/permiso', 'permiso');
+
+Route::get('permiso/{nombre}', function ($nombre) {
+    return $nombre;
+})->where('nombre','[A-Za-z]+')->name('permiso');
